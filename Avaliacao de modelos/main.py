9e3409f9fd2b56987c1ea9a2db81f25ad3c3030e
Alysson Hyago <alysson.oliveira@ee.ufcg.edu.br>
2022-10-19 10:47:17 -0300

    add pate final de avaliação de algoritmos

--- Avaliacao de modelos/main.py
+++ Avaliacao de modelos/main.py
@@ -165,4 +165,66 @@ shapiro(resultados_random)
 sns.displot(resultados_arvore,kind='kde')
 sns.displot(resultados_random, kind='kde')
 
-#Teste ANOVA
+#Teste ANOVA e Tukey
+
+from scipy.stats import f_oneway
+
+_, p = f_oneway(resultados_arvore, resultados_random)
+print(p)
+
+alpha = 0.05
+
+if p<= alpha:
+    print('Hipotese nula rejeitada. Dados são diferentes')
+else:
+    print('HIpotese alternativa rejeitada. resultados são iguais') # tanto faz usar qualquer um. caso fosse esse.
+
+# Salvando um modelo
+#### Carregando so dados do pré-processamento
+
+file = open('./Bases de dados/credit.pkl', 'rb')
+X_credit_treinamento, y_credit_treinamento, X_credit_teste, y_credit_teste = pickle.load(file)
+
+X_credit_treinamento.shape, y_credit_treinamento.shape
+
+# concatenar as duas variaveis de treinamento e teste
+
+X_credit = np.concatenate((X_credit_treinamento, X_credit_teste), axis=0)
+
+X_credit.shape
+
+y_credit = np.concatenate((y_credit_treinamento, y_credit_teste), axis=0)
+
+y_credit.shape
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+
+classificador_rede_neural = MLPClassifier(activation='relu', batch_size = 56, solver='adam')
+classificador_rede_neural.fit(X_credit, y_credit)
+
+classificador_arvore = DecisionTreeClassifier(criterion='entropy', min_samples_leaf=1, min_samples_split=5, splitter='best')
+classificador_arvore.fit(X_credit, y_credit)
+
+classificador_svm = SVC(C = 2.0, kernel='rbf', probability=True)
+classificador_svm.fit(X_credit, y_credit)
+
+import pickle
+pickle.dump(classificador_rede_neural, open('rede_neural_finalizado.sav', 'wb'))
+pickle.dump(classificador_arvore, open('arvore_finalizado.sav', 'wb'))
+pickle.dump(classificador_svm, open('svm_finalizado.sav', 'wb'))
+
+#Carregar um classificdor já treinado
+
+rede_neural = pickle.load(open('./Modelos salvos/rede_neural_finalizado.sav', 'rb'))
+arvore = pickle.load(open('./Modelos salvos/arvore_finalizado.sav', 'rb'))
+svm = pickle.load(open('./Modelos salvos/svm_finalizado.sav', 'rb'))
+
+novo_registro = X_credit[0]
+novo_registro = novo_registro.reshape(1,-1)
+novo_registro.shape
+
+rede_neural.predict(novo_registro)
+arvore.predict(novo_registro)
+svm.predict(novo_registro)
